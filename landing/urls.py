9bf5ba29496dde3a91fd65dd0c8ca10efa36cf2d
Borcuha69 <borcuha69@gmail.com>
2018-02-15 14:01:48 +0300

    Changed 15.02.2018

--- landing/urls.py
+++ landing/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     url(r'^landing/$', views.landing, name='landing'),
+    url(r'^landing2/$', views.landing2, name='landing2'),
 ]
--- landing/views.py
+++ landing/views.py
@@ -9,3 +9,11 @@ def landing(request):
         data = form.cleaned_data
         new_form = form.save()
     return render(request, 'landing/landing.html', locals())
+
+
+def landing2(request):
+    form = SubscriberForm(request.POST or None)
+    if request.method == 'POST' and form.is_valid():
+        data = form.cleaned_data
+        new_form = form.save()
+    return render(request, 'landing/landing2.html', locals())
